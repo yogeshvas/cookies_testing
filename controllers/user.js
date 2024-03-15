fb9e5edca8c981ccc9bc024cb16e04ed4e146e79
Yogesh Vashisth <130190342+yogeshvas@users.noreply.github.com>
2024-03-15 23:31:14 +0530

    updated middleware

--- controllers/user.js
+++ controllers/user.js
@@ -22,7 +22,7 @@ export const login = async (req, res, next) => {
   }
 };
 
-export const register = async (req, res) => {
+export const register = async (req, res,next) => {
   try {
     const { name, email, password } = req.body;
 
