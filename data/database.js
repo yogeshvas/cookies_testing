020f58fcf481e10e24c7425e70b6f78d57aadffd
Yogesh Vashisth <130190342+yogeshvas@users.noreply.github.com>
2024-03-15 22:08:51 +0530

    Update database.js

--- data/database.js
+++ data/database.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
-      dbName: "backendapi",
-    })
+    .connect("mongodb+srv://khair:khair123@cluster0.mu3sawn.mongodb.net/library")
     .then((c) => console.log(`Database Connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
